570436c391d9cbba985b9e80500381a324551475
AjayGhimire <ajayghimire1998@gmail.com>
2022-03-05 18:30:02 +1100

    Added Up and Down Keys events as well

--- index.js
+++ index.js
@@ -14,8 +14,26 @@ function moveDodgerRight () {
     const leftNumbers = dodger.style.left.replace('px', '');
     const left = parseInt(leftNumbers, 10);
 
-    if (left > 0) {
+    if (left < 360 ) {
         dodger.style.left = `${left + 1}px`;
+    }
+}
+
+function moveDodgerUp() {
+    const upNumbers = dodger.style.bottom.replace('px', '');
+    const bottom = parseInt(upNumbers, 10);
+
+    if (bottom< 380) {
+        dodger.style.bottom = `${bottom + 1}px`;
+    }
+}
+
+function moveDodgerDown() {
+    const upNumbers = dodger.style.bottom.replace('px', '');
+    const bottom = parseInt(upNumbers, 10);
+
+    if (bottom > 0 ) {
+        dodger.style.bottom = `${bottom -1}px`
     }
 }
 
@@ -26,6 +44,12 @@ document.addEventListener('keydown', function(e) {
     }
     else if ( e.key === 'ArrowRight') {
         moveDodgerRight();
+    }
+    else if (e.key === 'ArrowUp') {
+        moveDodgerUp();
+    } 
+    else if ( e.key === 'ArrowDown') {
+        moveDodgerDown();
     }
     
 });
